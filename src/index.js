beab473513481c5940fa05cc862f8e6e7478ca2e
Drew <thesimpledev@gmail.com>
2019-03-21 21:44:35 -0700

    Add diversity

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import SunnyDays from './sunnyDays'
 import MedianHomeCost from './medianHomeCost'
+import Diversity from './diversity'
 
 document.addEventListener("DOMContentLoaded", () => {
   const sunnyDays = new SunnyDays
   const medianHomeCost = new MedianHomeCost
+  const diversity = new Diversity
 })
