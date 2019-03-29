ef79ad23779ce4d84d0f687bb7dbc4bde5e5e905
Drew <thesimpledev@gmail.com>
2019-03-29 01:09:56 -0700

    Filter ticks

--- src/index.js
+++ src/index.js
@@ -14,4 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const map = new Map
   const jobMarket = new JobMarket
   const salary = new Salary
+
+  // filter ticks
+  let ticks = d3.selectAll('.tick text')
+  .attr('class', function(d) {
+    let city = this.textContent
+    if (parseInt(city) || ["White", "Asian", "Hispanic", "Black"].includes(city)) return ""
+    let cityClass = this.textContent.split(' ').join('-')
+    return `city city-${cityClass}`
+  })
 })
