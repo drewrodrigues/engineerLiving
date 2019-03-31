e06d95ccc68f4b8f753b1e86cffad6469ef8b606
Drew <thesimpledev@gmail.com>
2019-03-31 16:19:57 -0700

    Modularize job market

--- src/jobMarket.js
+++ src/jobMarket.js
@@ -20,6 +20,12 @@ import {
 
 class JobMarket {
   constructor() {
+    this.setData()
+    this.setChart()
+    this.render()
+  }
+
+  setData() {
     this.data = CITIES
 
     // indeed search: software engineer w/ exact location only 3/24/2019
@@ -33,80 +39,82 @@ class JobMarket {
     this.data[RALEIGN].jobs = 1377 // FIXME: RALEIGH
     this.data[DENVER].jobs = 1973
     this.data[PHOENIX].jobs = 1491
+  }
 
-    // total = 41,733
-    this.render()
+  setChart() {
+    const svg = d3.select('svg.jobMarket')
+      .attr('height', HEIGHT + MARGINS * 2).attr('width', WIDTH + MARGINS * 2)
+    this.chart = svg.append('g')
+      .attr('transform', `translate(${MARGINS + 70}, ${MARGINS + 100})`)
   }
 
   render() {
-    const svg = d3.select('svg.jobMarket')
-      .attr("height", HEIGHT + MARGINS * 2).attr("width", WIDTH + MARGINS * 2)
-    const chart = svg.append('g')
-      .attr("transform", `translate(${MARGINS + 70}, ${MARGINS + 100})`)
-    const radius = Math.min(WIDTH, HEIGHT) / 2
+    this._setChartVariables()
+    this._addPieChart()
+    this._addCityLabel()
+    this._addCountLabel()
+  }
 
-    var pie = d3.pie()
+  _setChartVariables() {
+    this.radius = Math.min(WIDTH, HEIGHT) / 2
+
+    this.pie = d3.pie()
       .value(d => d.jobs)
-    
-    let arc = d3.arc()
+
+    this.arc = d3.arc()
       .innerRadius(0)
-      .outerRadius(radius)
-    
-    let labelArc = d3.arc()
-      .innerRadius(radius)
-      .outerRadius(radius + 110)
+      .outerRadius(this.radius)
 
-    let percentageArc = d3.arc()
-      .innerRadius(radius / 2)
-      .outerRadius(radius)
+    this.labelArc = d3.arc()
+      .innerRadius(this.radius)
+      .outerRadius(this.radius + 110)
 
-    // pie chart
-    chart
+    this.percentageArc = d3.arc()
+      .innerRadius(this.radius / 2)
+      .outerRadius(this.radius)
+  }
+
+  _addPieChart() {
+    this.chart
       .selectAll()
-      .data(pie(Object.values(this.data)))
+      .data(this.pie(Object.values(this.data)))
       .enter()
       .append('path')
       .attr('class', d => `city ${d.data.class}`)
-      .transition()
-        .ease(ANIMATION_EASING)
-        .delay((d, i) => i * ANIMATION_DELAY)
-        .duration(ANIMATION_DURATION)
-      .attr('d', arc)
+      .attr('d', this.arc)
       .attr('fill', d => d.data.color)
-  
-    // city label
-    chart
+  }
+
+  _addCityLabel() {
+    this.chart
       .selectAll()
-      .data(pie(Object.values(this.data)))
+      .data(this.pie(Object.values(this.data)))
       .enter()
-      .append("text")
+      .append('text')
       .attr('class', d => `city ${d.data.class}`)
       .attr('text-anchor', 'middle')
-      .attr("transform",  d => `translate(${labelArc.centroid(d)})`)
+      .attr('transform',  d => `translate(${this.labelArc.centroid(d)})`)
+      .text(d => d.data.city)
       .transition()
         .ease(ANIMATION_EASING)
         .delay((d, i) => i * ANIMATION_DELAY)
         .duration(ANIMATION_DURATION)
-        .text(d => d.data.city)
-        .attr("transform", function(d) { 
-          var midAngle = d.endAngle < Math.PI ? d.startAngle/2 + d.endAngle/2 : d.startAngle/2  + d.endAngle/2 + Math.PI
-          return `translate(${labelArc.centroid(d)[0]}, ${labelArc.centroid(d)[1]}) rotate(-95) rotate(${midAngle * 180/Math.PI})`;
+        .attr('transform', d => { 
+          let midAngle = d.endAngle < Math.PI ? d.startAngle/2 + d.endAngle/2 : d.startAngle/2  + d.endAngle/2 + Math.PI
+          return `translate(${this.labelArc.centroid(d)[0]}, ${this.labelArc.centroid(d)[1]}) rotate(-95) rotate(${midAngle * 180/Math.PI})`;
         })
-    
-    // percentage label
-    chart
+  }
+
+  _addCountLabel() {
+    this.chart
       .selectAll()
-      .data(pie(Object.values(this.data)))
+      .data(this.pie(Object.values(this.data)))
       .enter()
-      .append("text")
+      .append('text')
       .attr('class', d => `city ${d.data.class} city-data-toggle`)
       .attr('text-anchor', 'middle')
-      .attr("transform",  d => `translate(${percentageArc.centroid(d)})`)
-      .transition()
-        .ease(ANIMATION_EASING)
-        .delay((d, i) => i * ANIMATION_DELAY)
-        .duration(ANIMATION_DURATION)
-        .text(d => d.data.jobs)
+      .attr('transform',  d => `translate(${this.percentageArc.centroid(d)})`)
+      .text(d => d.data.jobs)
   }
 }
 
